c4a3d99415e2202ae2132c1f881cc24428a0cfd1
pi0trecom <skorupa.piotr91@gmail.com>
2017-12-19 20:20:45 +0100

    małe poprawki

--- src/main/java/pl/sdacademy/views/InvoiceView.java
+++ src/main/java/pl/sdacademy/views/InvoiceView.java
@@ -8,15 +8,5 @@ import java.util.List;
  * Created by marcin on 13.12.2017.
  */
 public class InvoiceView {
-    public static void printSalesInvioces(List<Invoice> salesInvoicesList){
-        for (Invoice invoice : salesInvoicesList){
-            System.out.println("Faktura sprzedarzowa: " + invoice.toString());
-        }
-    }
 
-    public static void printPurchaseInvioces(List<Invoice> purchaseInvoicesList){
-        for (Invoice invoice : purchaseInvoicesList){
-            System.out.println("Faktura zakupowa: " + invoice.toString());
-        }
-    }
 }
